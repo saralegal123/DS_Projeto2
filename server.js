ee01f0d65a870036040236c810557c441ed255ea
coletti <matheus.coletti08@gmail.com>
2025-10-10 22:24:40 -0300

    DS - Projeto 2 / criação das pastar controller, services e repositories

--- server.js
+++ server.js
@@ -1,27 +1,16 @@
 const express = require("express");
+const productController = require("./controller/product.controller");
 
 const server = express();
 server.use(express.json);
 const port = 3000;
 
-server.get("/products", (request, response) => {
-    response.json(productsList);
-});
-
-server.post("/products", (request, response) => {
-    const newProduct = request.body;
-    const newId = productsList.length + 1;
-
-    newProduct.id = newId;
-    productsList.push(newProduct);
-
-    response.status(201).json(newProduct)
-
-});
+server.get("/products", productController.list);
+server.post("/products", productController.create);
 
 //rota da api
 server.get("/", (request, response) => {
-    response.send("Oi Sara linda!")
+    response.send("Oi, Sara linda!")
 });
 
 server.listen(port, () => {
